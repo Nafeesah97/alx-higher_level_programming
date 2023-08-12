3f51c8913646ec1fb1563cf699f67c750bb46489
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-12 10:47:20 -0700

    4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 import sys, hidden_4
-res = dir(hidden_4)
-for item in res:
-    if "__" in item:
-        pass
-    else:
-        print("{}".format(item))
+if __name__ == "__main__":
+    res = dir(hidden_4)
+    for item in res:
+        if "__" in item:
+            pass
+        else:
+            print("{}".format(item))
