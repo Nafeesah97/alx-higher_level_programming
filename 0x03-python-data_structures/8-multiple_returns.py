3cb0c7ee65e39cb02f4b67d67003cb702482b743
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-14 16:41:10 -0700

    8-multiple_returns.py

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+    length = len(sentence)
+    first = sentence[0]
+    if len(sentence) == 0:
+        first = None
+    result = (length, first)
+    return result
