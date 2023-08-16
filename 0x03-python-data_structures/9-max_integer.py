6a8c91a82c85d174cb775e57acd6e5d50cb60484
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-16 14:07:39 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+    else:
+        for i in range(len(my_list)):
+            j = 0
+            while j < i:
+                if my_list[j] > my_list[j + 1]:
+                    temp = my_list[j]
+                    my_list[j] = my_list[j + 1]
+                    my_list[j + 1] = temp
+                j += 1
+        return my_list[-1]
