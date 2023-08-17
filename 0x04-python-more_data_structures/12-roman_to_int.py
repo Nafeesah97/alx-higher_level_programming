37c4be520445c377d69f0d879dda58db572afe75
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-17 06:34:33 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
-        return None
+        return 0
     standard_roman = dict([('I', 1), ('IV', 4), ('V', 5), ('IX', 9), ('X', 10), ('XL', 40), ('L', 50), ('XC', 90), ('C', 100), ('CD', 400), ('D', 500), ('CM', 900), ('M', 1000)])
     total = 0
     i = 0
