3d213c325f7afe8c756165edcf9b375d86c05c55
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-17 06:41:22 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,11 @@
 def roman_to_int(roman_string):
     if roman_string is None or not isinstance(roman_string, str):
         return 0
-    standard_roman = dict([('I', 1), ('IV', 4), ('V', 5), ('IX', 9), ('X', 10), ('XL', 40), ('L', 50), ('XC', 90), ('C', 100), ('CD', 400), ('D', 500), ('CM', 900), ('M', 1000)])
+    standard_roman = dict([
+        ('I', 1), ('IV', 4), ('V', 5), ('IX', 9), ('X', 10), ('XL', 40),
+        ('L', 50), ('XC', 90), ('C', 100), ('CD', 400), ('D', 500),
+        ('CM', 900), ('M', 1000)
+        ])
     total = 0
     i = 0
 
@@ -12,6 +16,6 @@ def roman_to_int(roman_string):
             total += standard_roman[roman_string[i:i+2]]
             i += 2
         else:
-             total += standard_roman[roman_string[i]]
-             i += 1
+            total += standard_roman[roman_string[i]]
+            i += 1
     return total
