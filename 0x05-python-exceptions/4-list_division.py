b0dad6957de8f3477e2a7ac2a48aa91f309ca694
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-29 15:51:07 -0700

    4-list_division.py

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    res_list = []
+    try:
+        for i, j in zip(my_list_1, my_list_2):
+            try:
+                res = i / j
+
+            except TypeError:
+                res = 0
+                print("wrong type")
+            except IndexError:
+                res = 0
+                print("out of range")
+            except ZeroDivisionError:
+                res = 0
+                print("division by 0")
+            else:
+                res_list.append(res)
+        if len(res_list) < list_length:
+            print("out of range")
+            res_list.extend([0] * (list_length - len(res_list)))
+    except Exception:
+        pass
+    finally:
+        return res_list
