1d955d943b2ebdb300aca7a229de9ade30ccd9e8
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-08-29 16:41:47 -0700

    5-raise_exception.py

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 
 def raise_exception():
-    try:
-        value = "I am a girl"
-        int_val = int(value)
-    except Exception as e:
-        raise type(e)
+    raise TypeError
