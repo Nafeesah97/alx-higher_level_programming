93e8c74f79c6889b0d08f7aba7ceb5a5e473287c
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-01 10:04:49 -0700

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -4,5 +4,13 @@ class Square:
     """
     defines an empty square
 
+    Attributes:
+        None
+
+    Methods:
+        None
+
+    Example:
+        >>> my_square = Square()
     """
     pass
