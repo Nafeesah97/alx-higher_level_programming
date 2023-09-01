0f6239a9d44994d06f92328f4f4b4c9503047ae3
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-01 10:29:34 -0700

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module defines the size of Square class
+
+"""
+
+
+class Square:
+    """
+    A class representing an empty square
+
+    Attributes:
+        size(int): The size of square
+
+    Method:
+        None
+
+    Example:
+        >>> my_square = Square(3)
+
+    """
+    def __init__(self, size):
+        self.__size = size
