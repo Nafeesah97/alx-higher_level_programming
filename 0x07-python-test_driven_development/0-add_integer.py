3a2bee4a827420db4c447b5e4c50d4ad729c6414
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-02 16:37:50 -0700

    testing

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+This module for performing mathematical operations
+This module provides functions for addition
+Author: Nafeesah
+"""
+def add_integer(a, b=98):
+    """
+    Adds 2 integers.
+    """
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    result = int(a) + int(b)
+    return result
+
+if __name__ = '__main__':
+    import doctest
+    doctest.testfile()
