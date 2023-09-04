8e22148be104a86238d73394129144ad2ad0c8b1
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-04 06:54:57 -0700

    2-rectangle.py

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class Rectangle
+Author: nafeesah
+"""
+
+
+class Rectangle:
+    """
+    Represents a rectangle
+
+    Attributes:
+        width(int): Width of the rectangle
+        height(int): The height of the rectangle
+
+    Methods:
+        area(): Finds the area of rectangle
+        perimeter(): Finds the perimeter of rectangle
+    """
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        perimeter = 0
+        if self.width == 0 or self.height == 0:
+            return perimeter
+        else:
+            perimeter = (self.width * 2) + (self.height * 2)
+            return perimeter
