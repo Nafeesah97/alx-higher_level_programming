9281c7f58133d6ee3d33f03067b9b8152791ccda
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-13 15:01:05 -0700

    an area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class
+A class know as BaseGeometry
+Author: Nafeesah
+"""
+
+
+class BaseGeometry():
+
+    """
+    A class named BaseGeometry.
+
+    Attribute:
+        None
+
+    Method:
+        area(): computes area of geometry
+    """
+    def area(self):
+        """To check area of geometry"""
+        raise Exception("area() is not implemented")
