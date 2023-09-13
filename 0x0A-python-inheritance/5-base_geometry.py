de2e310ffc328a2a26e3cb6d53febe872631f085
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-13 14:53:14 -0700

    empty class

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class
+A class know as BaseGeometry
+Author: Nafeesah
+"""
+
+
+class BaseGeometry():
+
+    """
+    A class named BaseGeometry.
+
+    Attribute:
+        None
+
+    Method:
+        None
+    """
+    pass
