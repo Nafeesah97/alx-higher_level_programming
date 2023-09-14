745c285733a81c7a965f8e3bf15945ec4cad8c6c
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-15 00:32:28 +0100

    read file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,6 +6,7 @@ that wirte/overwrites something into a file
 Author: Nafeesah
 """
 
+
 def write_file(filename="", text=""):
     """writes a text into a file"""
     with open(filename, mode="w", encoding="UTF-8") as f:
