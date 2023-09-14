be026ec1119250032297514fda5ef1edfbea19f0
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-15 00:04:51 +0100

    write file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+""""
+This module contains a funcrion
+that wirte/overwrites something into a file
+Author: Nafeesah
+"""
+
+def write_file(filename="", text=""):
+    """writes a text into a file"""
+    with open(filename, mode="w", encoding="UTF-8") as f:
+        f.write(text)
