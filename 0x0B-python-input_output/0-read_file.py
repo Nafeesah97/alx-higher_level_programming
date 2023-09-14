8ce804b26eb99c6c236d495cf65099f8a2705357
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-14 23:22:40 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains a function that reads a file
+to stout
+Author: Nafeesah
+"""
+
+def read_file(filename=""):
+    """A function that reads file"""
+    with open("filename", mode="r", encoding="UTF-8") as f:
+        print(f.read())
