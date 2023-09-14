9cb5cab28e63d6db34ade3e91a05c86e980d616b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-14 09:22:09 -0700

    square str

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class
+A class know as BaseGeometry
+Author: Nafeesah
+"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """
+    class Square that inherits from Rectangle.
+
+    Attributes:
+        size(int): length of rectangle
+
+    Methods:
+        area(): area of square
+    """
+
+    def __init__(self, size):
+        """to initialize size"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """calculate the area"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        return f"[Square] {self.__size}/{self.__size}"
