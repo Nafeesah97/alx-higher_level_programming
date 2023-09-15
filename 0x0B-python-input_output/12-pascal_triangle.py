810b604f9d12c71460c2438159a07cd66ea26c80
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-16 00:05:28 +0100

    pascal triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+"""
+This module contains function for pascal triangle
+"""
+
+
+def pascal_triangle(n):
+    """returns a list of lists of integers representing Pascals triangle"""
+    res = []
+    p = []
+    for i in range(n):
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                p.append(1)
+            else:
+                previous_row = res[i - 1]
+                p.append(previous_row[j - 1] + previous_row[j])
+        res.append(p)
+    return res
