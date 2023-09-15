070824a19d908184f3d3399782d474e74c6249ea
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-15 22:05:43 +0100

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+importing json to serialize
+"""
+import json
+
+"""
+This module contains a function
+that converts  to class from json
+Author: Nafeesah
+"""
+
+
+def class_to_json(obj):
+    """function to convert instance attributes to json """
+    data = obj.__dict__
+    json_data = json.dumps(data)
+    return json_data
