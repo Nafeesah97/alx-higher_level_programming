de86ea8e37cff4c0a3ccaf4cd9d9d9559337ff0b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-15 22:59:29 +0100

    student json2

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+This module contains a class that contains a function
+that converts  to class from json
+Author: Nafeesah
+"""
+
+
+class Student:
+    """
+    a class called student
+
+    Attributes:
+        first_name(str): The first name of student
+        last_name(str): The family name of student
+        age(int): The age of student
+
+        Methods:
+            to_json(): retrieves a dictionary representation of a Student
+    """
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """a function for class to json"""
+        json_dat = {}
+        if type(attrs) is list:
+            for item in attrs:
+                json_dat.append(item)
+            return json_dat
+        return (self.__dict__)
