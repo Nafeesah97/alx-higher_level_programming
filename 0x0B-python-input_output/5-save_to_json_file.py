95c92b5b8437707f0a43bdc3a034dba8a6229099
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-15 14:24:01 +0100

    json and write

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -14,10 +14,7 @@ Author: Nafeesah
 def save_to_json_file(my_obj, filename):
     """A function to convert to json and write into a file"""
     try:
-        data = json.dumps(my_obj)
-        with open(filename, mode="w", encoding="UTF-8") as f:
-            text = f.write(data)
-            return text
+        with open(filename, mode="w") as file:
+            json.dump(my_obj, file)
     except:
         pass
-    
