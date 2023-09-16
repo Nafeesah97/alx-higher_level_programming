3ea5b2fce71d7ea3daec23ad8692a4a8a093e208
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-16 17:38:59 +0100

    rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -42,7 +42,7 @@ class Rectangle(Base):
     """
     def __init__(self, width, height, x=0, y=0, id=None):
         """To initialize class instance attributes"""
-        super().__init__(self, id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
