778c3a5d9c8d518cb6642918390c58e191a0ad2d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-16 16:40:48 +0100

    base class

--- 0x0C-python-almost_a_circle/README.md
+++ 0x0C-python-almost_a_circle/README.md
@@ -1,1 +1,15 @@
 0x0C-python-almost_a_circle
+This a project to practice the following concept:
+Import
+Exceptions
+Class
+Private attribute
+Getter/Setter
+Class method
+Static method
+Inheritance
+Unittest
+Read/Write file
+args and kwargs
+Serialization/Deserialization
+JSON
---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+This module contains the class base that
+is used to instantiate the id
+Author: Nafeesah
+"""
+
+
+class Base:
+    """
+    This is class named Base.
+
+    Attributes:
+        nb_objects(int): number of instances
+        id(int): id of class instances
+
+    Methods:
+        None
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """To instantiate object attributes"""
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
