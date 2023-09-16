de468f5c1ab10649970ebf6dcb83c8dc801256fc
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-16 17:30:13 +0100

    rectangle

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python3
+
+"""
+This module contains a class named rectangle
+Author: Nafeesah
+"""
+
+
+class Base:
+    """
+    This is class named Base.
+
+    Attributes:
+        nb_objects(int): number of instances
+        id(int): id of class instances
+
+    Methods:
+        None
+    """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """To instantiate object attributes"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+class Rectangle(Base):
+    """
+    A class named rectangle
+
+    Attributes:
+        width(int): width of rectangle
+        height(int): height of rectangle
+        x(int): 
+        y(int):
+    
+    Methods:
+        None
+    """
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """To initialize class instance attributes"""
+        super().__init__(self, id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        return self.__width
+    
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
+    @property
+    def height(self):
+        return self.__height
+    
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        return self.__x
+    
+    @x.setter
+    def x(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        return self.__y
+    
+    @y.setter
+    def y(self, value):
+        self.__y = value
