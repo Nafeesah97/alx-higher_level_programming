92333f4d16fbd8e715ad7f18970779a33556e1bb
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-16 18:43:15 +0100

    rectangle str

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -108,3 +108,7 @@ class Rectangle(Base):
         """To display rectangle"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        """Magic method to overwrite"""
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
