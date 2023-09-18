2e196ab2ff4a3d5ed7f49eeed9a8e7749c2d8ad1
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-19 00:37:37 +0100

    testing2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -57,10 +57,7 @@ class Base:
     @classmethod
     def create(cls, **dictionary):
         """ returns an instance with all attributes already set"""
-        if cls.__name__ == "Square":
-            obj = cls(1)
-        else:
-            obj = cls(1, 1)
+        obj = cls(1)
         obj.update(**dictionary)
         return obj
     
