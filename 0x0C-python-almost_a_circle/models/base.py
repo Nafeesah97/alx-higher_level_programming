fbf22ab6ece072527e3d3e3350ba1f98043d7830
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-19 00:52:41 +0100

    testing2

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -40,6 +40,8 @@ class Base:
     @classmethod
     def save_to_file(cls, list_objs):
         """writes the JSON string representation of list_objs to a file"""
+        if list_objs is None:
+            list_objs = []
         filename = f"{cls.__name__}.json"
         with open(filename, "w") as file:
             file.write(cls.to_json_string([obj.to_dictionary()
