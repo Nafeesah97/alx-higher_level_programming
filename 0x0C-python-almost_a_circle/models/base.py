4275fa9a26c3626a4b099e6a91dd0ee811a314a5
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-18 15:27:03 +0100

    base create and json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """to serialise/deserialise"""
 import json
+import os
 """
 This module contains the class base that
 is used to instantiate the id
@@ -52,3 +53,41 @@ class Base:
                 string = cls.to_json_string(list_dict)
 
                 file.write(string)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """returns the list of the JSON string representation"""
+        if json_string is None or json_string == []:
+            string_data = []
+        else:
+            string_data = json.loads(json_string)
+        return string_data
+
+    @classmethod
+    def create(cls, **dictionary):
+        """ returns an instance with all attributes already set"""
+        if issubclass(cls, Base):
+            if cls.__name__ == "Base":
+                return Base(dictionary.get("id", None))
+            
+            if cls.__name__ == "Square":
+                obj = cls(1)
+            else:
+                obj = cls(1, 1)
+
+            obj.update(**dictionary)
+            return obj
+    
+    @classmethod
+    def load_from_file(cls):
+        """returns a list of instances"""
+        file_path = f"{cls.__name__}.json"
+        if not os.path.exists(file_path):
+            output = []
+            return output
+
+        with open(f"{cls.__name__}.json", "r") as f:
+            json_data = f.read()
+            json_dic = cls.from_json_string(json_data)
+            for i in json_dic:
+                return (cls.create(**i))
