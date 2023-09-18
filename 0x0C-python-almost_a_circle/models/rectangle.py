ccd4eb74c233e18d3fde20093ef7ef013b25a31e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-18 01:21:22 +0100

    rec json file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,31 +1,10 @@
 #!/usr/bin/python3
-
+from models.base import Base
 """
 This module contains a class named rectangle
 Author: Nafeesah
 """
 
-
-class Base:
-    """
-    This is class named Base.
-
-    Attributes:
-        nb_objects(int): number of instances
-        id(int): id of class instances
-
-    Methods:
-        None
-    """
-    __nb_objects = 0
-
-    def __init__(self, id=None):
-        """To instantiate object attributes"""
-        if id is not None:
-            self.id = id
-        else:
-            Base.__nb_objects += 1
-            self.id = Base.__nb_objects
 
 class Rectangle(Base):
     """
