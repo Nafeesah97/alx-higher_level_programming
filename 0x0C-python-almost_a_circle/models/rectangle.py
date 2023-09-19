aa9b16f9dd9f1f256eede760165516fbd1d3d135
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-09-19 01:30:06 +0100

    testing2

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,9 +13,9 @@ class Rectangle(Base):
     Attributes:
         width(int): width of rectangle
         height(int): height of rectangle
-        x(int): 
+        x(int):
         y(int):
-    
+
     Methods:
         None
     """
@@ -30,25 +30,24 @@ class Rectangle(Base):
     @property
     def width(self):
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        
         if value <= 0:
             raise ValueError("width must be > 0")
         self.__width = value
-    
+
     @property
     def height(self):
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        
+
         if value <= 0:
             raise ValueError("height must be > 0")
         self.__height = value
@@ -56,12 +55,12 @@ class Rectangle(Base):
     @property
     def x(self):
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         if type(value) is not int:
             raise TypeError("x must be an integer")
-        
+
         if value < 0:
             raise ValueError("x must be >= 0")
         self.__x = value
@@ -69,7 +68,7 @@ class Rectangle(Base):
     @property
     def y(self):
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         if type(value) is not int:
@@ -82,7 +81,7 @@ class Rectangle(Base):
     def area(self):
         """To calculate area of rectangle"""
         return (self.__height * self.__width)
-    
+
     def display(self):
         """To display rectangle"""
         if self.__y > 0:
@@ -93,7 +92,10 @@ class Rectangle(Base):
 
     def __str__(self):
         """Magic method to overwrite"""
-        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+        return (
+            f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - "
+            f"{self.__width}/{self.__height}"
+        )
 
     def update(self, *args, **kwargs):
         """assigns an argument to each attribute"""
