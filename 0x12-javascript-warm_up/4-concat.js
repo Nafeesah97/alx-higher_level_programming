87ef6f5d5d7d072211053034c5b6c3c6cf6a8c49
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-13 15:30:32 -0800

    sen fixed

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-let sentence = args[0].concat(' is');
-let newSen = sentence.concat(args[1]);
+const newSen = args[0] + ' is ' + args[1];
 console.log(newSen);
