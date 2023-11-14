5e878a87cbb0821c36aee6bb1d1f20bba518ccbb
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-14 01:25:40 -0800

    love fixed

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const word = 'C is fun'
+const word = 'C is fun';
 const num = Number(args[0]);
 let i = 0;
 if (isNaN(num)) {
-    console.log('Missing number of occurrences');
-  } else {
-    while (i < num) {
-        console.log(word);
-        i++;
-    }
+  console.log('Missing number of occurrences');
+} else {
+  while (i < num) {
+    console.log(word);
+    i++;
+  }
 }
