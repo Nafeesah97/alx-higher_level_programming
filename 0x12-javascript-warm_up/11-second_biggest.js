9b1f358889d48a3ed93c795c5ebfeaf13aa7a30f
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-14 04:15:57 -0800

    second

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 const args = process.argv.slice(2);
 
 if (args.length <= 1) {
-    console.log (0);
+  console.log(0);
 } else {
-    const num = args.map(Number);
-    const sorNum = num.sort((a, b) => a - b);
-    console.log(sorNum[1]);
+  const num = args.map(Number);
+  const sorNum = num.sort((a, b) => a - b);
+  console.log(sorNum[1]);
 }
