12c2414ca5639db32d7bde076da642b4e792bb58
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-14 11:08:42 +0100

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,9 +4,9 @@ const num = Number(args[0]);
 
 function factorial (num) {
   if (isNaN(num) || num === 0 || num === 1) {
-	  return (1);
+    return (1);
   } else {
-	  return (num * factorial(num - 1));
+    return (num * factorial(num - 1));
   }
 }
 
