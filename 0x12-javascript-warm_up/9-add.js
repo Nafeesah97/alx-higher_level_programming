0b340dbb00f3db15f7e507a01ff58bd4cb79a96e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-14 01:51:16 -0800

    add fixed

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-const num1 = args[0];
-const num2 = args[1];
+const num1 = Number(args[0]);
+const num2 = Number(args[1]);
 
-function add(a, b) {
-    return (a + b);
+function add (a, b) {
+  return (a + b);
 }
 
-add(num1, num2);
+const res = add(num1, num2);
+console.log(res);
