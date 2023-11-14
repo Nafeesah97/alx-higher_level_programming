114cad673de5e9e3f62d59f18c07c70e6d403cd7
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-14 02:07:08 -0800

    fac fixing

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,11 +2,12 @@
 const args = process.argv.slice(2);
 const num = Number(args[0]);
 
-function factorial(num) {
-    if (num === 'NaN' || num === 0 || num === 1) {
-        return (1);
-    }
-    return (num * factorial(num - 1));
+function factorial (num) {
+  if (isNaN(num) || num === 0 || num === 1) {
+	  return (1);
+  } else {
+	  return (num * factorial(num - 1));
+  }
 }
 
 const res = factorial(num);
