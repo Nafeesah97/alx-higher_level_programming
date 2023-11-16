ca1ce5eee98158371ccf3a2eb38a736417b1308a
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-16 02:29:07 -0800

    convert

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-    function converter(num) {
-        num.toString(base);
-    }
-    converter();
-}
+  return function (num) {
+    return num.toString(base);
+  };
+};
