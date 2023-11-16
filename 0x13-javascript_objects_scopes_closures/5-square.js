2e009fe5828ef3532f6ad42d41f7efa2b31f2c31
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-16 01:39:42 +0100

    square

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+class Rectangle {
+    constructor (w, h) {
+      if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
+        return `${this.constructor.name} {}`;
+      }
+      this.width = w;
+      this.height = h;
+    }
+  
+    print () {
+      let i = 0;
+      while (i < this.height) {
+        console.log('X'.repeat(this.width));
+        i++;
+      }
+    }
+  
+    rotate () {
+      const h = this.height;
+      const w = this.width;
+      this.width = h;
+      this.height = w;
+    }
+  
+    double () {
+      this.width = this.width * 2;
+      this.height = this.height * 2;
+    }
+  }
+
+
+  module.exports = Rectangle;
+
+class Square extends Rectangle {
+    constructor(size) {
+        super();
+        this.height = size;
+        this.width = size;
+    }
+}
+  module.exports = Square;
