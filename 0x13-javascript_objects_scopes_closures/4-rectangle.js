896737c3c67a2c8571f92665af18ac0f64a46aaa
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-15 16:34:29 -0800

    task 4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
-        return `${this.constructor.name} {}`;
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
+      return `${this.constructor.name} {}`;
     }
+    this.width = w;
+    this.height = h;
+  }
 
-    print() {
-        let i = 0;
-        while (i < this.height) {
-            console.log('X'.repeat(this.width));
-            i++;
-        }
-    }
-
-    rotate() {
-        this.width = h;
-        this.height = w;
-    }
-
-    double() {
-        dou_width = this.width * 2;
-        dou_height = this.height * 2;
-        return dou_height, dou_width;
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
     }
   }
-  
-  module.exports = Rectangle;
-  
+
+  rotate () {
+    const h = this.height;
+    const w = this.width;
+    this.width = h;
+    this.height = w;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
