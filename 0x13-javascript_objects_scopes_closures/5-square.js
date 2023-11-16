5c3e2954e244b4289b0deb2209c40dfe9690dbe3
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-15 16:42:04 -0800

    square fixed

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,43 +1,42 @@
 #!/usr/bin/node
 
 class Rectangle {
-    constructor (w, h) {
-      if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
-        return `${this.constructor.name} {}`;
-      }
-      this.width = w;
-      this.height = h;
+  constructor (w, h) {
+    if (!Number.isInteger(w) || w <= 0 || !Number.isInteger(h) || h <= 0) {
+      return `${this.constructor.name} {}`;
     }
-  
-    print () {
-      let i = 0;
-      while (i < this.height) {
-        console.log('X'.repeat(this.width));
-        i++;
-      }
-    }
-  
-    rotate () {
-      const h = this.height;
-      const w = this.width;
-      this.width = h;
-      this.height = w;
-    }
-  
-    double () {
-      this.width = this.width * 2;
-      this.height = this.height * 2;
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    let i = 0;
+    while (i < this.height) {
+      console.log('X'.repeat(this.width));
+      i++;
     }
   }
 
+  rotate () {
+    const h = this.height;
+    const w = this.width;
+    this.width = h;
+    this.height = w;
+  }
 
-  module.exports = Rectangle;
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+}
+
+module.exports = Rectangle;
 
 class Square extends Rectangle {
-    constructor(size) {
-        super();
-        this.height = size;
-        this.width = size;
-    }
+  constructor (size) {
+    super();
+    this.height = size;
+    this.width = size;
+  }
 }
-  module.exports = Square;
+module.exports = Square;
