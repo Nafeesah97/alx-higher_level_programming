e17c6120d37e4f1bf1fcc3572205109743673e35
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-15 22:52:40 -0800

    X or C

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,21 +1,19 @@
 #!/usr/bin/node
-const Square = require('./5-square');
+const SquareBase = require('./5-square');
 
-class Square extends Square {
-    constructor() {
-        super();
+class Square extends SquareBase {
+  charPrint (c) {
+    const char = c;
+    if (char === undefined) {
+      super.print();
+    } else {
+      let i = 0;
+      while (i < this.height) {
+        console.log(char.repeat(this.width));
+        i++;
+      }
     }
-
-    charPrint(c) {
-        let char = c;
-        if (char === undefined) {
-            const x_print = super.print();
-        } else {
-            let i = 0;
-            while (i < this.height) {
-                console.log(char.repeat(this.width));
-                i++;
-            }
-        }
-    }
+  }
 }
+
+module.exports = Square;
