f7238dd965b9743ff4838d0972a4c71691f0f143
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-19 00:04:02 +0100

    filter state3

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Importing the necessary libraries
+"""
+import sys
+import MySQLdb
+"""
+This module is to list all the rows that state starts with N
+It lists it in ascending order by states.id
+Author: Nafeesah
+"""
+if __name__ == "__main__":
+    arg = sys.argv
+    mysql_username = arg[1]
+    mysql_password = arg[2]
+    database_name = arg[3]
+    word = arg[4]
+
+    db = MySQLdb.connect(
+            host='localhost', user=mysql_username, passwd=mysql_password,
+            db=database_name, port=3306
+            )
+    cursor = db.cursor()
+    cursor.execute(
+            "SELECT id, name FROM states WHERE name = '{}'\
+            ORDER BY id".format(word)
+            )
+    rows = cursor.fetchall()
+    for row in rows:
+        print('{}'.format(row))
+    cursor.close()
+    db.close()
