327bc58860a7f405fdc09be7aa311cfc25d38930
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-18 14:37:09 -0800

    done with task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -14,7 +14,10 @@ mysql_username = arg[1]
 mysql_password = arg[2]
 database_name = arg[3]
 
-db = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+db = MySQLdb.connect(
+        host='localhost', user=mysql_username, passwd=mysql_password,
+        db=database_name, port=3306
+        )
 cursor = db.cursor()
 cursor.execute("SELECT id, name FROM states ORDER BY id")
 rows = cursor.fetchall()
