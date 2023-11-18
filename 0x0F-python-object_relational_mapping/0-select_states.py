eaa642a67ef8ba41213d38ed86d3dde73c6f20b8
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-18 14:34:02 -0800

    done with task 1

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,18 +10,15 @@ It lists it in ascending order by states.id
 Author: Nafeesah
 """
 arg = sys.argv
-mysql_username = arg[0]
-mysql_password = arg[1]
-database_name = arg[2]
+mysql_username = arg[1]
+mysql_password = arg[2]
+database_name = arg[3]
 
-db = MySQLdb.connect(host=localhost, user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
+db = MySQLdb.connect(host='localhost', user=mysql_username, passwd=mysql_password, db=database_name, port=3306)
 cursor = db.cursor()
-cursor.execute("SELECT id, name FROM state ORDER BY id")
+cursor.execute("SELECT id, name FROM states ORDER BY id")
 rows = cursor.fetchall()
 for row in rows:
-    for col in row:
-        print("{}".format(col))
-    print("\n")
-
+    print('{}'.format(row))
 cursor.close()
 db.close()
