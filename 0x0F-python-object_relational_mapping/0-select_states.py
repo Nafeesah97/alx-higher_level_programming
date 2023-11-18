96e5c702709e59d1d2075c1f9391d71af9f09779
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-18 23:45:07 +0100

    all state2

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,19 +9,20 @@ This module is to list all the rows in the state
 It lists it in ascending order by states.id
 Author: Nafeesah
 """
-arg = sys.argv
-mysql_username = arg[1]
-mysql_password = arg[2]
-database_name = arg[3]
+if __name__ == "__main__":
+    arg = sys.argv
+    mysql_username = arg[1]
+    mysql_password = arg[2]
+    database_name = arg[3]
 
-db = MySQLdb.connect(
-        host='localhost', user=mysql_username, passwd=mysql_password,
-        db=database_name, port=3306
-        )
-cursor = db.cursor()
-cursor.execute("SELECT id, name FROM states ORDER BY id")
-rows = cursor.fetchall()
-for row in rows:
-    print('{}'.format(row))
-cursor.close()
-db.close()
+    db = MySQLdb.connect(
+            host='localhost', user=mysql_username, passwd=mysql_password,
+            db=database_name, port=3306
+            )
+    cursor = db.cursor()
+    cursor.execute("SELECT id, name FROM states ORDER BY id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print('{}'.format(row))
+    cursor.close()
+    db.close()
