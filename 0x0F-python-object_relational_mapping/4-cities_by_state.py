dbb552b51f0331f8b10bfb7544665bc9559dee3e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-19 00:37:09 +0100

    cities

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Importing the necessary libraries
+"""
+import sys
+import MySQLdb
+"""
+This module is to list all the rows in the state
+It lists it in ascending order by states.id
+Author: Nafeesah
+"""
+if __name__ == "__main__":
+    arg = sys.argv
+    mysql_username = arg[1]
+    mysql_password = arg[2]
+    database_name = arg[3]
+
+    db = MySQLdb.connect(
+            host='localhost', user=mysql_username, passwd=mysql_password,
+            db=database_name, port=3306
+            )
+    cursor = db.cursor()
+    cursor.execute("SELECT a.id, a.name, b.name FROM cities AS a\
+                   LEFT JOIN states AS b\
+                   ON a.state_id = b.id\
+                   ORDER BY a.id")
+    rows = cursor.fetchall()
+    for row in rows:
+        print('{}'.format(row))
+    cursor.close()
+    db.close()
