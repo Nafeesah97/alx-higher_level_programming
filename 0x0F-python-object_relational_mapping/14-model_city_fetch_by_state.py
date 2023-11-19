d0b66ff453006701d7937ccaebd95b657ff441d7
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-19 07:40:26 -0800

    cities

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,8 +5,6 @@ import sys
 from model_state import Base, State
 from model_city import City
 from sqlalchemy.orm import sessionmaker
-
-
 from sqlalchemy import (create_engine)
 
 if __name__ == "__main__":
@@ -22,4 +20,4 @@ if __name__ == "__main__":
              .outerjoin(State, City.state_id == State.id)
              .order_by(City.id))
     for city, state in query.all():
-        print('{}: {} {}'.format(state.name, city.id, city.name))
+        print('{}: ({}) {}'.format(state.name, city.id, city.name))
