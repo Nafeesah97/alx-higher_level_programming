8436afafa1eefa97612d8f0d407264346073cb69
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-18 16:46:57 -0800

    model fixed

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -6,7 +6,7 @@ from sqlalchemy import MetaData, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 """
-This module contains a script that contains 
+This module contains a script that contains
 the class definition of a State
 Author: Nafeesah
 """
@@ -15,10 +15,10 @@ Author: Nafeesah
 mymetadata = MetaData()
 Base = declarative_base(metadata=mymetadata)
 
+
 class State(Base):
     """
     A class of state
-    
     Attrubutes:
         id(int): represent state id
         name(str): represents nameof state
