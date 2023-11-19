f2015ce571639408d322110519800d872287df0d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-11-19 16:12:23 +0100

    cities

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+import sys
+from model_state import Base, State
+from model_city import City
+from sqlalchemy.orm import sessionmaker
+
+
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost/{}'
+            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+            pool_pre_ping=True
+            )
+    Session = sessionmaker()
+    Session.configure(bind=engine)
+    session = Session()
+    query = (session.query(City, State)
+             .outerjoin(State, City.state_id == State.id)
+             .order_by(City.id))
+    for city, state in query.all():
+        print('{}: {} {}'.format(state.name, city.id, city.name))
---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Importing the necessary libraries
+"""
+from sqlalchemy import MetaData, Column, Integer, String
+from model_state import Base
+
+"""
+This module contains a script that contains
+the class definition of a State
+Author: Nafeesah
+"""
+
+
+class City(Base):
+    """
+    A class of cities
+    Attrubutes:
+        id(int): represent cities id
+        name(str): represents name of cities
+        state_id(int): represents the state id
+    Methods:
+        None
+    """
+    __tablename__ = 'cities'
+    id = Column(Integer, autoincrement=True, nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'))
