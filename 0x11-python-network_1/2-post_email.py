c6c455c29239ceb33c752029df922d4a293b187f
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-12-01 18:02:11 -0800

    email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""POST request to the passed URL with the email as a parameter"""
+import sys
+import urllib.request as req
+import urllib.parse
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    pos = req.Request(url, data)
+    with req.urlopen(pos) as response:
+        html = response.read()
+        res = html.decode('utf-8')
+    print('Your email is: {}'.format(res[email]))
