f4de5eb095f79c039062ba35b3cddbd2b2cd559b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2023-12-01 17:15:06 -0800

    fet

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,5 +2,13 @@
 """fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request as req
 
-with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
-    html = response.read()
+if __name__ == "__main__":
+    with req.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+    print("Body response:")
+    print("\t- type: ", end="")
+    print(type(html))
+    print("\t- content: ", end="")
+    print(html)
+    print("\t- utf8 content: ", end="")
+    print(html.decode("utf-8"))
