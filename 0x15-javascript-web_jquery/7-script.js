8e3579ffbf8a4e7ccb2f4ad026f9495d51d8431d
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-01-11 20:21:23 +0100

    name

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,10 @@
+$(function() {
+    var $character = $('#character')
+    $.ajax({
+        type: 'GET',
+        url: 'https://swapi-api.alx-tools.com/api/people/5/?format=json',
+        success: function(character) {
+            $character.text(character.name)
+        }
+    });
+});
